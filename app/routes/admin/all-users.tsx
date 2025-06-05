b35e79c27050d254176e8cf726996c79620d05cd
DaBekCarRie <98639247+DaBekCarRie@users.noreply.github.com>
2025-06-05 13:17:25 +0700

    dashboard layout

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,8 +1,15 @@
+import { Header } from 'components'
 import React from 'react'
 
 const AllUsers = () => {
   return (
-    <div>AllUsdsdsers</div>
+    <main className='dashboard wrapper'>
+      <Header
+      title="Trips Page"
+      description="Check out our current users in real time"
+      />
+      All Users Page Contents
+    </main>
   )
 }
 
